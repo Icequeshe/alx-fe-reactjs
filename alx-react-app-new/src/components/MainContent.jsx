31a53979eb8c2e8db0d7bf1379e8e01bcba3ad9c
Charity Nduta <charitynduta29@gmail.com>
2025-07-22 23:31:26 +0300

    Update MainContent.jsx

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -2,7 +2,7 @@ function MainContent() {
   return (
     <main style={{ padding: '20px', backgroundColor: '#e0f7fa', minHeight: '200px' }}>
       <p style={{ fontSize: '1.2rem', color: '#333' }}>
-        Welcome to the list of my favorite cities around the world!
+        I love to visit New York, Paris, and Tokyo.
       </p>
     </main>
   );
