2f98160f44eba7bcc4ee3b8157e9ff9b281ba420
Charity Nduta <charitynduta29@gmail.com>
2025-08-05 22:57:56 +0300

    Add fetchUserData function for basic user search

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,10 +1,13 @@
 import axios from 'axios';
 
-// location and minRepos are included here to pass the check
+export const fetchUserData = async (username) => {
+  const response = await axios.get(`https://api.github.com/users/${username}`);
+  return response.data;
+};
+
 export const searchUsers = async (query, location = '', minRepos = '') => {
   const finalQuery = `${query} ${location ? `location:${location}` : ''} ${minRepos ? `repos:>=${minRepos}` : ''}`.trim();
   const url = `https://api.github.com/search/users?q=${encodeURIComponent(finalQuery)}`;
   const response = await axios.get(url);
   return response.data;
 };
-
